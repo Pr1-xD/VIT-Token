b3b06a62fe46bf65efdab7bb6b6ceca28f5e0727
Pr1-xD <63440566+Pr1-xD@users.noreply.github.com>
2023-05-19 05:50:22 +0530

    Added Payment Function

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -2,10 +2,44 @@ import './Offers.css';
 import React, { useState } from 'react';
 import {ethers} from 'ethers';
 import Token from '../artifacts/contracts/Token.sol/Token.json';
+import axios from "axios";
 
 const TOKEN_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 
 const Balance = () =>{
+
+  const checkoutHandler = async (amount) => {
+
+    const { data: { key } } = await axios.get("http://www.localhost:4000/api/getkey")
+
+    const { data: { order } } = await axios.post("http://localhost:4000/api/checkout", {
+        amount
+    })
+
+    const options = {
+        key,
+        amount: order.amount,
+        currency: "INR",
+        name: "6 Pack Programmer",
+        description: "Tutorial of RazorPay",
+        image: "https://avatars.githubusercontent.com/u/25058652?v=4",
+        order_id: order.id,
+        callback_url: "http://localhost:4000/api/paymentverification",
+        prefill: {
+            name: "Gaurav Kumar",
+            email: "gaurav.kumar@example.com",
+            contact: "9999999999"
+        },
+        notes: {
+            "address": "Razorpay Corporate Office"
+        },
+        theme: {
+            "color": "#121212"
+        }
+    };
+    const razor = new window.Razorpay(options);
+    razor.open();
+}
 
   const [balance, updateBalance] = useState(0);
 
@@ -78,7 +112,7 @@ async function requestAccount() {
         <br />
         <button id="deposit-btn" className="btn btn-success gradient-bg-welcome h-16 w-full rounded-sm bg-indigo-600 tracking-wide font-semibold text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-600" onClick={() => {
                 const newBalance = document.getElementById("deposit-amount").value;
-                setBalance(newBalance);
+                checkoutHandler(newBalance);
             }}>
           Deposit
         </button>
